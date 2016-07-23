df851eea15cf436b595c2c8937788051b9be8855
Damian Lajara <damiansito93@gmail.com>
2016-07-22 23:04:51 -0400

    flip sprites on animation

--- states/MainMenu.js
+++ states/MainMenu.js
@@ -4,6 +4,8 @@ BasicGame.MainMenu = function (game) {
 	this.bg;
 	this.cursors;
 	this.player;
+	this.jumpButton;
+	this.jumpTimer = 0;
 
 };
 
@@ -15,17 +17,19 @@ BasicGame.MainMenu.prototype = {
 		this.player = this.game.add.sprite(90, 500, 'player');
 		this.player.anchor.setTo(0.5, 0.5);
 		this.player.scale.setTo(0.18, 0.18);
-		this.player.animations.add('idle', [ 1, 5, 7, 8, 2, 9, 14, 15, 3, 16 ], 3, true);
+		this.player.animations.add('idle', [ 1, 5, 7, 8, 2, 9, 14, 15, 3, 16 ], 4, true);
 		this.player.animations.add('walk', [ 31, 32, 33, 13, 20, 35, 36, 37, 39, 38 ], 8, true);
 		this.player.animations.add('run',  [ 18, 0, 12, 26, 28, 29, 30, 6, 27 , 34 ], 8, true);
-		this.player.animations.add('jump', [ 10, 17, 21, 22, 23, 24, 4, 11, 25 , 19 ], 8, true);
+		this.player.animations.add('jump', [ 10, 17, 21, 22, 23, 24, 4, 11, 25 , 19 ], 8, false);
 
 
-
+		this.game.physics.startSystem(Phaser.Physics.ARCADE);
 		this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+		this.game.physics.arcade.gravity.y = 350;
+
 		this.player.enableBody = true;
 		this.player.body.allowGravity=true;
-		this.player.body.drag.set(40);
+		this.player.body.drag.set(30);
 		this.player.body.maxVelocity.set(500);
 		this.player.body.collideWorldBounds = true;
 		this.player.body.width = 90;
@@ -33,45 +37,53 @@ BasicGame.MainMenu.prototype = {
 
 		this.game.camera.follow(this.player);
 		this.cursors = this.game.input.keyboard.createCursorKeys();
+		this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 	},
 
 	update: function () {
-
-		//	Do some nice funky main menu effect here
-		if (this.cursors.left.isDown) {
-
-			this.player.body.acceleration.x = -40;
-			this.move_player();
-			// this.player.animations.play('run');
-
-		} else if (this.cursors.up.isDown) {
-
-			this.player.body.acceleration.y = -40;
-			this.player.animations.play('jump');
-
-		} else if (this.cursors.right.isDown) {
-
-			this.player.body.acceleration.x = 40;
-			this.move_player();
-			// this.player.animations.play('walk');
-
-		} else {
-			this.player.body.acceleration.x = 0;
-			this.player.body.acceleration.y = 0;
-			this.move_player();
-			// this.player.animations.play('idle');
-		}
-
+		this.move_player();
 	},
 
 	move_player: function() {
-		if(this.player.body.acceleration.x == 0 && this.player.body.acceleration.y == 0) {
-			this.player.animations.play('idle');
-		} else if (this.player.body.acceleration.y < 200 || (this.player.body.acceleration.x < 200 || this.player.body.acceleration.x > 200)) {
-			this.player.animations.play('run');
+		// Implement Running Functionality
+		this.player.body.velocity.x = 0;
+
+		if (this.cursors.left.isDown) {
+			this.player.body.acceleration.x = -50;
+			this.player.body.velocity.x = -150;
+
+			if (this.facing != 'left') {
+				this.facing = 'left';
+				this.player.scale.setTo(-0.18, 0.18);
+				this.player.animations.play('walk');
+			}
+		} else if (this.cursors.right.isDown) {
+			this.player.body.acceleration.x = 150;
+			this.player.body.velocity.x = 150;
+
+			if (this.facing != 'right') {
+				this.facing = 'right';
+				this.player.scale.setTo(0.18, 0.18);
+				this.player.animations.play('walk');
+			}
 		} else {
-			this.player.animations.play('walk');
+			if (this.facing != 'idle') {
+				this.player.animations.play('idle');
+
+				if (this.facing == 'left') {
+					this.player.scale.setTo(-0.18, 0.18);
+				} else {
+					this.player.scale.setTo(0.18, 0.18);
+				}
+				this.facing = 'idle';
+			}
+		}
+
+		if (this.jumpButton.isDown && this.player.body.onFloor() && this.game.time.now > this.jumpTimer) {
+				this.player.animations.play('jump');
+				this.player.body.velocity.y = -250;
+				this.jumpTimer = this.game.time.now + 750;
 		}
 	},
 
@@ -101,3 +113,57 @@ BasicGame.MainMenu.prototype = {
 	}
 
 };
+
+
+
+
+
+// this.player.body.velocity.x = 0;
+//
+// if (this.cursors.left.isDown)
+// {
+// 	this.player.body.acceleration.x = -50;
+// 	this.player.body.velocity.x = -150;
+//
+//
+// 	if (this.facing != 'left')
+// 	{
+// 		this.player.animations.play('left');
+// 		this.facing = 'left';
+// 	}
+// }
+// else if (this.cursors.right.isDown)
+// {
+// 	this.player.body.acceleration.x = 150;
+// 	this.player.body.velocity.x = 150;
+//
+// 	if (this.facing != 'right')
+// 	{
+// 		this.player.animations.play('right');
+// 		this.facing = 'right';
+// 	}
+// }
+// else
+// {
+// 	if (this.facing != 'idle')
+// 	{
+// 		this.player.animations.stop();
+//
+// 		if (this.facing == 'left')
+// 		{
+// 			this.player.frame = 0;
+// 		}
+// 		else
+// 		{
+// 			this.player.frame = 5;
+// 		}
+//
+// 		this.facing = 'idle';
+// 	}
+// }
+//
+// if (this.jumpButton.isDown && this.player.body.onFloor() && this.game.time.now > this.jumpTimer)
+// {
+// 	this.player.body.velocity.y = -250;
+// 	this.jumpTimer = game.time.now + 750;
+// }
